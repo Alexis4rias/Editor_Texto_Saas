c8ae06ea2f7274038d57514dcb2f156f130b663b
Alexis Arias <jalexis2004arias@gmail.com>
2026-09-17 22:20:36 -0600

    Se creó un Login

--- src/test/java/um/edu/mx/editor_texto_saas/EditorTextoSaasApplicationTests.java
+++ src/test/java/um/edu/mx/editor_texto_saas/EditorTextoSaasApplicationTests.java
@@ -1,13 +1,26 @@
 package um.edu.mx.editor_texto_saas;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EditorTextoSaasApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		try (Connection connection = dataSource.getConnection()) {
+			assertEquals("EDITOR_USER", connection.getMetaData().getUserName());
+		}
 	}
 
 }
